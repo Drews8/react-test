5a48a2935305937017f27904f78d013c66dc87dd
Drews8 <prokhorenko.drew@gmail.com>
2020-02-08 12:55:56 +0200

    create main structure

---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import style from "./Aside.module.css"
+
+const Aside = () => {
+    return (
+    <div className={style.aside}></div>
+    )};
+
+export default Aside;
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import style from "./Body.module.css"
+
+const Body = () => {
+    return (
+        <div className={style.body}></div>
+    )};
+
+export default Body;
